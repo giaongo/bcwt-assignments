e936ebc8c624b33b4904c4806f96da5d0a3f13ca
Giao Ngo <giao.ngo@metropolia.fi>
2022-11-09 16:38:08 +0200

    complete front-end JS task 2

--- front-end-js-tasks/task2-ajax/script.js
+++ front-end-js-tasks/task2-ajax/script.js
@@ -21,22 +21,51 @@ const getTVSeriesData = async(name) => {
         console.log("results:",data);
         renderResult(data);
     } catch(error) {
-        console.log("Network fails");
+        console.log("Network fails",error);
     }
-
 } 
 
 const renderResult = (data) => {
     results.innerHTML = "";
     for(let i=0; i < data.length; i++) {
-        const h3 = document.createElement("h3");
-        h3.innerText = data[i].show.name;
-        const img = document.createElement("img");
-        img.src = data[i].show.image.medium;
-        results.append(h3);
-        results.append(img);
+       renderDataIntoResult(data[i],results);
     } 
 }
+
+const renderDataIntoResult = (data,results) => {
+    const h3 = document.createElement("h3");
+    h3.innerText = data.show.name;
+    const img = document.createElement("img");
+    img.src = data.show.image ? data.show.image.medium : "http://placekitten.com/200/300";
+    const officialSite = document.createElement("a");
+    const siteLink = data.show.officialSite ? data.show.officialSite : "#No-Link"
+    officialSite.href = siteLink;
+    officialSite.innerText = siteLink;
+    officialSite.style.display = "block";
+    const summary = document.createElement("p");
+    summary.innerHTML = data.show.summary;
+    results.append(h3);
+    results.append(img);
+    results.append(officialSite);
+    results.append(summary);
+    renderGenres(data.show.genres,results)
+}
+
+const renderGenres = (genresList,results) => {
+    const genres = document.createElement("div");
+    for(let j = 0; j < genresList.length; j++) {
+        const genre = document.createElement("span");
+        genre.innerText = genresList[j];
+        if(j !== (genresList.length - 1)) {
+            genre.innerText += " | ";
+        } else {
+            genre.innerHTML += "<hr><br>";
+        }
+        genres.append(genre);
+    }
+    results.append(genres);
+}
+
 document.addEventListener("click",(event) => {
     console.log("mouse clicked somewhere on the page",event);
 })
