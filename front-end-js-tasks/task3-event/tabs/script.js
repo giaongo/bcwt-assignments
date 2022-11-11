50471a85e987c01ab34c0865cc71d069bc4619c7
Giao Ngo <giao.ngo@metropolia.fi>
2022-11-11 17:26:12 +0200

    complete task3-event tabs

---
+++ front-end-js-tasks/task3-event/tabs/script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const tabs = document.querySelector("tab-panel");
+const tabList = [];
+
+const displayTabContent = (activeTab) => {
+  const previousActiveTab = tabList.find((tabElement) =>
+    tabElement.button.classList.contains("activeTab")
+  );
+  if (previousActiveTab) {
+    previousActiveTab.button.classList.remove("activeTab");
+    previousActiveTab.content.style.display = "none";
+    previousActiveTab.content.classList.remove("activeTab");
+  }
+  activeTab.content.style.display = "block";
+  activeTab.content.classList.add("activeTab");
+};
+
+const changeTab = (tabList) => {
+  for (let i = 0; i < tabList.length; i++) {
+    const currentButton = tabList[i].button;
+    currentButton.addEventListener("click", () => {
+      displayTabContent(tabList[i]);
+      currentButton.classList.add("activeTab");
+    });
+  }
+};
+
+function asTabs(node) {
+  const tabContainer = document.createElement("div");
+  tabContainer.className = "tabContainer";
+  tabs.before(tabContainer);
+  Object.values(node.children).forEach((child) => {
+    const button = document.createElement("button");
+    button.textContent = child.getAttribute("data-tabname");
+    button.id = button.textContent;
+    tabContainer.appendChild(button);
+    tabList.push({ button, content: child });
+    child.style.display = "none";
+  });
+  changeTab(tabList);
+}
+
+asTabs(tabs);
