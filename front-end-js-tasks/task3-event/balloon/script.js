a695a0bd1426d5274bf3ba709e28d1df889e8b2d
Giao Ngo <giao.ngo@metropolia.fi>
2022-11-11 17:33:39 +0200

    reformat code in task3-balloon

--- front-end-js-tasks/task3-event/balloon/script.js
+++ front-end-js-tasks/task3-event/balloon/script.js
@@ -1,26 +1,26 @@
-"use strict"
-const balloon = document.querySelector("#balloon")
-const MAX_BALLOON_SIZE = 400
-const MIN_BALLOON_SIZE = 30
-let currentSize = MIN_BALLOON_SIZE
-balloon.style.fontSize = currentSize + "px"
-balloon.style.margin = 0
+"use strict";
+const balloon = document.querySelector("#balloon");
+const MAX_BALLOON_SIZE = 400;
+const MIN_BALLOON_SIZE = 30;
+let currentSize = MIN_BALLOON_SIZE;
+balloon.style.fontSize = currentSize + "px";
+balloon.style.margin = 0;
 
-const isBalloonAtMaxLimit = (size) => size >= MAX_BALLOON_SIZE
+const isBalloonAtMaxLimit = (size) => size >= MAX_BALLOON_SIZE;
 const handleKeyListener = (event)  => {
-    event.preventDefault()
+    event.preventDefault();
     if (isBalloonAtMaxLimit(currentSize)) {
-        balloon.innerText = "💥"
-        window.removeEventListener("keydown",handleKeyListener)
+        balloon.innerText = "💥";
+        window.removeEventListener("keydown",handleKeyListener);
     } else {
         if (event.key === "ArrowUp") {
-            currentSize *= 1.1 
+            currentSize *= 1.1 ;
         } else if(event.key === "ArrowDown") {
-            currentSize *= 0.9
+            currentSize *= 0.9;
         }
-       balloon.style.fontSize = currentSize + "px"
+       balloon.style.fontSize = currentSize + "px";
     }
 
 }
 
-window.addEventListener("keydown",(event) => handleKeyListener(event))
+window.addEventListener("keydown",(event) => handleKeyListener(event));
