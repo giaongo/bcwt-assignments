570dda4ba583fe9832f9852f0d537b2d08ea885d
Giao Ngo <giao.ngo@metropolia.fi>
2022-11-11 17:35:32 +0200

    reformat the code in task3 mouse trail

--- front-end-js-tasks/task3-event/mouse-trail/script.js
+++ front-end-js-tasks/task3-event/mouse-trail/script.js
@@ -1,4 +1,4 @@
-let trailData = []
+let trailData = [];
 let mouse = {
     x:0,
     y:0
@@ -6,35 +6,35 @@ let mouse = {
 
 const createTrail = () => {
     for(let i = 0; i < 10; i++) {
-        const trail = document.createElement("div")
-        trail.className = "trail"
-        trailData.push(trail)
+        const trail = document.createElement("div");
+        trail.className = "trail";
+        trailData.push(trail);
     }
 }
 window.addEventListener("mousemove",event  => {
-    mouse.x =  event.pageX 
-    mouse.y  = event.pageY
+    mouse.x =  event.pageX;
+    mouse.y  = event.pageY;
 })
 
 // Function to move trail
 const moveTrail = () => {
-    let x = mouse.x
-    let y = mouse.y
+    let x = mouse.x;
+    let y = mouse.y;
     for(let i = 0; i < trailData.length;i++) {
-        const currentTrail = trailData[i]
-        const nextTrail = trailData[i+1] || trailData[0]
-        currentTrail.style.left = x + "px"
-        currentTrail.style.top = y + "px"
-        document.body.appendChild(currentTrail)
-        x += (nextTrail.offsetLeft - currentTrail.offsetLeft) *0.8
-        y += (nextTrail.offsetTop - currentTrail.offsetTop)*0.8
+        const currentTrail = trailData[i];
+        const nextTrail = trailData[i+1] || trailData[0];
+        currentTrail.style.left = x + "px";
+        currentTrail.style.top = y + "px";
+        document.body.appendChild(currentTrail);
+        x += (nextTrail.offsetLeft - currentTrail.offsetLeft) *0.8;
+        y += (nextTrail.offsetTop - currentTrail.offsetTop)*0.8;
     }
 }
 
 // Function to activate screen repaint
 const animate = () => {
-    moveTrail()
-    requestAnimationFrame(animate)
+    moveTrail();
+    requestAnimationFrame(animate);
 }
-createTrail()
-animate()
+createTrail();
+animate();
