2ab3e25bdae233c4145951578662c7e1699436e9
Giao Ngo <giao.ngo@metropolia.fi>
2022-11-22 15:07:09 +0200

    complete session

--- state-management-example/app.js
+++ state-management-example/app.js
@@ -2,25 +2,64 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
-app.set('views', './views');
+const user = {
+  username: "foo",
+  password: "bar"
+}
+
+app.set('views','./views');
 app.set('view engine', 'pug');
 app.use(cookieParser())
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // form data
+app.use(session({
+  secret:"ghfdioigob",
+  saveUninitialized:false,
+  resave:true,
+  cookie:{maxAge:60000}
+}));
+
 
 app.get('/', (req, res) => {
   res.render('home');
 });
+app.get('/form', (req, res) => {
+  res.render('form');
+});
 
+app.get('/secret', (req, res) => {
+  if(req.session.loggedIn) {
+    res.render("secret");
+  } else {
+    res.redirect("/form");
+  }
+});
+
+app.post('/login', (req, res) => {
+  if(req.body.username == user.username && req.body.password == user.password) {
+    req.session.loggedIn = true;
+  } 
+  res.redirect("/secret");
+});
+app.get("/logout",(req,res) => {
+  req.session.loggedIn = false;
+  // res.clearCookie("connect.sid") cookie for the session
+  res.redirect("/");
+})
 app.get('/getCookie', (req, res) => {
   console.log(req.cookies);
   res.send("Your color choice was " + req.cookies.color);
 });
+
 app.get("/setCookie/:color",(req,res) => {
   console.log(req.params.color);
   res.cookie("color",req.params.color);
   res.send("cookie set");
 })
+
 app.get('/deleteCookie', (req, res) => {
   res.clearCookie("color")
   res.send("Color cookie deleted")
